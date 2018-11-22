f5eba9f4b59a0f0110b64b0ecf35e5a5d97a1e75
raihan_fajri <raihan@jojonomic.com>
2018-11-22 21:22:15 +0700

    Check in user

--- public/javascripts/log.js
+++ public/javascripts/log.js
@@ -39,37 +39,19 @@
   $('#page-log').change(function(){
     var page = $('#page-log').val()
     $("#tabel-log").html('')
-    for(var i = (page-1)*limit; i < (page*limit); i++){
+    for(var i = (page-1)*limit; i < ((page*limit) > log.length ? log.length : (page*limit)); i++){
       $("#tabel-log").append(
         '<tr>'+
-          '<td>'+ log[i].name +'</td>'+
-          '<td>'+ log[i].status +'</td>'+
-          '<td>'+ log[i].count_users +'</td>'+
-          '<td><a style="cursor:pointer" onClick="updateLog('+ i +')><i class="fa fa-edit"></a></i>&nbsp;&nbsp;&nbsp;<a style="cursor:pointer" onClick="deleteLog('+ i +')><i class="fa fa-trash-o"></i></a></td>'+
+          '<td>'+ log[i].user.name +'</td>'+
+          '<td>'+ log[i].user.role.name +'</td>'+
+          '<td>'+ log[i].tujuan +'</td>'+
+          '<td><span class="label label-'+ (log[i].status == 1 ? 'success' : 'danger') +'">'+ (log[i].status == 1 ? 'Masuk' : 'Keluar') +'</span></td>'+
+          '<td>'+ log[i].checkin_time +'</td>'+
+          '<td>'+ log[i].checkout_time +'</td>'+
+          '<td><a style="cursor:pointer" onClick="updateLog('+ i +')"><i class="fa fa-edit"></a></i></td>'+
         '</tr>'
       )
     }
-  })
-  $('#tambah-log').click(function(){
-    $("#modal-default").modal()
-    $("#modal-title").html('Tambah Log')
-    $("#modal-body").html(
-      '<div class="form-group">'+
-        '<label>Nama Log</label>'+
-        '<input id="nama-log" type="text" class="form-control" placeholder="Masukan nama log">'+
-      '</div>'+
-      '<div class="form-group">'+
-        '<label>Status</label>'+
-        '<select id="status-log" class="form-control">'+
-          '<option value="0">Permanent</option>'+
-          '<option value="1">Temporary</option>'+
-        '</select>'+
-      '</div>'
-    )
-    $('#modal-footer').html(
-      '<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>'+
-      '<button type="button" onClick="submitLog(0, 1)" class="btn btn-primary">Save changes</button>'
-    );
   })
   function submitLog(id, state){
     $("#modal-default").modal('hide');
@@ -82,14 +64,8 @@
     }
     var url = ''
     switch(state){
-      case 1 : 
-        url = '/api/role/create';
-        break;
       case 2 :
-        url = '/api/role/update';
-        break;
-      case 3 : 
-        url = '/api/role/delete';
+        url = '/api/log/update';
         break;
     }
     $.ajax({
@@ -108,32 +84,23 @@
     $("#modal-title").html('Edit Log')
     $("#modal-body").html(
       '<div class="form-group">'+
-        '<label>Nama Log</label>'+
-        '<input id="nama-log"  type="text" class="form-control" placeholder="Masukan nama log">'+
+        '<label>Nama</label>'+
+        '<input id="nama-log"  type="text" class="form-control" disabled>'+
       '</div>'+
       '<div class="form-group">'+
-        '<label>Status</label>'+
-        '<select id="status-log" class="form-control">'+
-          '<option value="0">Permanent</option>'+
-          '<option value="1">Temporary</option>'+
-        '</select>'+
+        '<label>Kriteria</label>'+
+        '<input id="Kriteria-log"  type="text" class="form-control" disabled>'+
+      '</div>'+
+      '<div class="form-group">'+
+        '<label>Tujuan</label>'+
+        '<input id="tujuan-log"  type="text" class="form-control" placeholder="Masukan Tujuan">'+
       '</div>'
     )
-    $('#nama-log').val(log[index].name)
-    $('#status-log').val(log[index].status == 'Permanent'? 0 : 1)
+    $('#tujuan-log').val((log[index].tujuan == '-'? null : log[index].tujuan))
+    $('#nama-log').val(log[index].user.name)
+    $('#kriteria-log').val(log[index].user.role.name)
     $('#modal-footer').html(
       '<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>'+
       '<button type="button" onClick="submitLog('+ log[index].id +', 2)" class="btn btn-primary">Save changes</button>'
-    );
-  }
-  function deleteLog(index){
-    $("#modal-default").modal()
-    $("#modal-title").html('Delete Log')
-    $("#modal-body").html(
-      'Apakah anda yakin untuk menghapus log <strong>'+ log[index].name + '</strong>'
-    )
-    $('#modal-footer').html(
-      '<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>'+
-      '<button type="button" onClick="submitLog('+ log[index].id +', 3)" class="btn btn-danger">Delete</button>'
     );
   }
